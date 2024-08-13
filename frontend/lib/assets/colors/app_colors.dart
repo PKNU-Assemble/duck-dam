b3191d6af49f12c086f9b34bcfbc383cb3932871
sunga <tjddk6662@naver.com>
2024-08-13 21:06:17 +0900

    feat : 하단 내비게이션 바 제작 완료
    lib/widgets/custom_bottom_navigation_bar.dart
    
    issues : #13

---
+++ frontend/lib/assets/colors/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xffffffff);
+  static const Color selectedColor = Color(0xa0a0a0a0);
+  static const Color noSelectedColor = Color(0xcccccccc);
+  static const Color backgroundColor = Color(0xd9d9d9d9);
+  // 더 많은 색상을 여기서 정의하세요.
+}
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/custom_bottom_navigation_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -110,6 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: const CustomBottomNavigationBar(),
     );
   }
 }
